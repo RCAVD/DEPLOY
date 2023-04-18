c692a52dfc5be0b8847204e7dc2f0e09b59c08a7
RCAVD <102368601+RCAVD@users.noreply.github.com>
2023-04-18 08:43:07 +0200

    Add files via upload

---
+++ ExportGetUserLogins.ps1
@@ -0,0 +1,3 @@
+﻿$hostname = hostname.exe
+$logfile = "\\NAS01\d$\RDSHLogs\$($hostname)-userlogin.log_$(get-date -Format dd-MM-yyyy_hhmm).txt"
+powershell "C:\Custom\GetUserLogins.ps1" > "$logfile" -append
